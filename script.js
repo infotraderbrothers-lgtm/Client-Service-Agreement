8e166a0a60fda82e03712bb7c488475ae3997eb6
infotraderbrothers-lgtm <infotraderbrothers@gmail.com>
2026-01-04 09:22:01 +0000

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let scopeOfWorkData = {};
 
 // API Configuration
 const WEBHOOK_URL = 'https://hook.eu2.make.com/b1xehsayp5nr7qtt7cybsgd19rmcqj2t';
-const PDFSHIFT_API_KEY = 'sk_b35232e4caa90a88ce18e7cf175498ed34180fd4';
+const PDFSHIFT_API_KEY = 'sk_b620db3746ace840fc2030d7ff07e49153afbde0';
 const PDFSHIFT_API_URL = 'https://api.pdfshift.io/v3/convert/pdf';
 
 // Initialize when page loads
